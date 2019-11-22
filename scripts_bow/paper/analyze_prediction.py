571ce6b401925af8e7bad37fd2c782e58ff2e1db
Georg Schramm <georg.schramm@kuleuven.be>
2019-11-22 12:35:32 +0100

    update analysis script for paper

--- scripts_bow/paper/analyze_prediction.py
+++ scripts_bow/paper/analyze_prediction.py
@@ -100,12 +100,24 @@ def roi_to_region(roi):
   return region
 
 #------------------------------------------------------------------------------------------------------------
+from argparse import ArgumentParser
+parser = ArgumentParser(description = 'boxplots of CNN Bowsher models')
 
-model_name = sys.argv[1]
-dataset    = sys.argv[2]
-osem_sdir  = sys.argv[3]
-osem_file  = sys.argv[4]
-bow_file   = sys.argv[5]
+parser.add_argument('model_name', help = 'model to analyze')
+
+parser.add_argument('--tracers', default = ['FDG','PE2I','FET'], 
+                                  help = 'data sets to analyze', nargs = '+')
+parser.add_argument('--osem_sdir', default = '20_min', help = 'osem count level')
+parser.add_argument('--osem_file', default = 'osem_psf_4_5.nii', help = 'osem file to use')
+parser.add_argument('--bow_file',  default = 'bow_bet_1.0E+01_psf_4_5.nii', help = 'bowsher file to use')
+
+args = parser.parse_args()
+
+model_name = args.model_name
+tracers    = args.tracers
+osem_sdir  = args.osem_sdir
+osem_file  = args.osem_file
+bow_file   = args.bow_file
 
 model_dir  = '../data/trained_models'
 recompute  = False
@@ -121,87 +133,89 @@ reg_results = pd.DataFrame()
 
 lps_flip = lambda x: np.flip(np.flip(x,0),1)
 
-if dataset == 'mmr-fdg':
-  mdir      = '../../data/test_data/mMR/Tim-Patients'
-  pdirs     = glob(os.path.join(mdir,'Tim-Patient-*'))
-elif dataset == 'signa-pe2i':
-  mdir      = '../../data/test_data/signa/signa-pe2i'
-  pdirs     = glob(os.path.join(mdir,'ANON????'))
-elif dataset == 'signa-fet':
-  mdir      = '../../data/test_data/signa/signa-fet'
-  pdirs     = glob(os.path.join(mdir,'ANON????'))
-
-for pdir in pdirs:
-  print(pdir)
-
-  output_dir      = os.path.join(pdir,'predictions',osem_sdir)
-  prediction_file = os.path.join(output_dir, '___'.join([os.path.splitext(model_name)[0],osem_file]))
-
-  # read the prediction
-  cnn_bow, voxsize   = read_nii(prediction_file)
-  bbox_data          = pickle.load(open(os.path.splitext(prediction_file)[0] + '_bbox.pkl','rb'))
-
-  bow, _    = read_nii(os.path.join(pdir,'20_min',bow_file))
-  osem, _   = read_nii(os.path.join(pdir,osem_sdir,osem_file))
-  aparc, _  = read_nii(os.path.join(pdir,aparc_file))
-  mr, _     = read_nii(os.path.join(pdir,mr_file))
-
-  # crop and interpolate
-  bow = bow[bbox_data['bbox']]
-  bow = zoom3d(bow, bbox_data['zoomfacs'])
-
-  osem = osem[bbox_data['bbox']]
-  osem = zoom3d(osem, bbox_data['zoomfacs'])
-
-  mr = mr[bbox_data['bbox']]
-  mr = zoom3d(mr, bbox_data['zoomfacs'])
-
-  aparc = aparc[bbox_data['bbox']]
-  aparc = zoom3d(aparc, bbox_data['zoomfacs'], interpolate = False)
-
-  df_file = os.path.splitext(prediction_file)[0] + '_regional_stats.csv'
-
-  if (not os.path.exists(df_file)) or recompute:
-    df             = regional_statistics(cnn_bow, bow, aparc)
-    df["subject"]  = os.path.basename(pdir)
-    df['roiname']  = df['roinum'].apply(lambda x: roilut[roilut.num == x].roi.to_string(index = False))
-    df['region']   = df["roiname"].apply(roi_to_region)
-    df['bow_file'] = bow_file
-    df             = df.reindex(columns=['subject','roinum','roiname','region','bow_file',
-                                         'mean','rc_mean','rmse','ssim'])
- 
-    df.to_csv(df_file) 
-    print('wrote: ', df_file)
-
-    # plot the results
-    lputamen_bbox = find_objects(lps_flip(aparc) == 12)
-    sl0 = int(0.5*(lputamen_bbox[0][0].start + lputamen_bbox[0][0].stop))
-    sl1 = int(0.5*(lputamen_bbox[0][1].start + lputamen_bbox[0][1].stop))
-    sl2 = int(0.5*(lputamen_bbox[0][2].start + lputamen_bbox[0][2].stop))
-
-    mr_imshow_kwargs  = {'vmin':0, 'vmax':np.percentile(mr,99.99), 'cmap':py.cm.Greys_r}
-    pet_imshow_kwargs = {'vmin':0, 'vmax':np.percentile(cnn_bow[aparc>0],99.99)}
-    vi = ThreeAxisViewer([lps_flip(mr),lps_flip(osem),lps_flip(bow),lps_flip(cnn_bow)], 
-                          sl_x = sl0, sl_y = sl1, sl_z = sl2, ls = '', rowlabels = ['T1 MR','OSEM','BOW','$BOW_{CNN}$'],
-                          imshow_kwargs = [mr_imshow_kwargs] + 3*[pet_imshow_kwargs])
-    vi.fig.savefig(os.path.splitext(prediction_file)[0] + '.png')
-    vi.fig_cb.savefig(os.path.splitext(prediction_file)[0] + '_cb.png')
-
-    py.close(vi.fig)
-    py.close(vi.fig_cb)
-    py.close(vi.fig_sl)
+for tracer in tracers:
+  if tracer == 'FDG':
+    mdir      = '../../data/test_data/mMR/Tim-Patients'
+    pdirs     = glob(os.path.join(mdir,'Tim-Patient-*'))
+  elif tracer == 'PE2I':
+    mdir      = '../../data/test_data/signa/signa-pe2i'
+    pdirs     = glob(os.path.join(mdir,'ANON????'))
+  elif tracer == 'FET':
+    mdir      = '../../data/test_data/signa/signa-fet'
+    pdirs     = glob(os.path.join(mdir,'ANON????'))
   else:
-    print('reading : ', df_file)
-    df = pd.read_csv(df_file)
- 
-  df['dataset'] = dataset 
-  reg_results = reg_results.append([df], ignore_index = True)
+    raise ValueError('Invalid tracer: ', tracer)
+
+  for pdir in pdirs:
+    print(pdir)
+  
+    output_dir      = os.path.join(pdir,'predictions',osem_sdir)
+    prediction_file = os.path.join(output_dir, '___'.join([os.path.splitext(model_name)[0],osem_file]))
+  
+    # read the prediction
+    cnn_bow, voxsize   = read_nii(prediction_file)
+    bbox_data          = pickle.load(open(os.path.splitext(prediction_file)[0] + '_bbox.pkl','rb'))
+  
+    bow, _    = read_nii(os.path.join(pdir,'20_min',bow_file))
+    osem, _   = read_nii(os.path.join(pdir,osem_sdir,osem_file))
+    aparc, _  = read_nii(os.path.join(pdir,aparc_file))
+    mr, _     = read_nii(os.path.join(pdir,mr_file))
+  
+    # crop and interpolate
+    bow = bow[bbox_data['bbox']]
+    bow = zoom3d(bow, bbox_data['zoomfacs'])
+  
+    osem = osem[bbox_data['bbox']]
+    osem = zoom3d(osem, bbox_data['zoomfacs'])
+  
+    mr = mr[bbox_data['bbox']]
+    mr = zoom3d(mr, bbox_data['zoomfacs'])
+  
+    aparc = aparc[bbox_data['bbox']]
+    aparc = zoom3d(aparc, bbox_data['zoomfacs'], interpolate = False)
+  
+    df_file = os.path.splitext(prediction_file)[0] + '_regional_stats.csv'
+  
+    if (not os.path.exists(df_file)) or recompute:
+      df             = regional_statistics(cnn_bow, bow, aparc)
+      df["subject"]  = os.path.basename(pdir)
+      df['roiname']  = df['roinum'].apply(lambda x: roilut[roilut.num == x].roi.to_string(index = False))
+      df['region']   = df["roiname"].apply(roi_to_region)
+      df['bow_file'] = bow_file
+      df             = df.reindex(columns=['subject','roinum','roiname','region','bow_file',
+                                           'mean','rc_mean','rmse','ssim'])
+   
+      df.to_csv(df_file) 
+      print('wrote: ', df_file)
+  
+      # plot the results
+      lputamen_bbox = find_objects(lps_flip(aparc) == 12)
+      sl0 = int(0.5*(lputamen_bbox[0][0].start + lputamen_bbox[0][0].stop))
+      sl1 = int(0.5*(lputamen_bbox[0][1].start + lputamen_bbox[0][1].stop))
+      sl2 = int(0.5*(lputamen_bbox[0][2].start + lputamen_bbox[0][2].stop))
+  
+      mr_imshow_kwargs  = {'vmin':0, 'vmax':np.percentile(mr,99.99), 'cmap':py.cm.Greys_r}
+      pet_imshow_kwargs = {'vmin':0, 'vmax':np.percentile(cnn_bow[aparc>0],99.99)}
+      vi = ThreeAxisViewer([lps_flip(mr),lps_flip(osem),lps_flip(bow),lps_flip(cnn_bow)], 
+                            sl_x = sl0, sl_y = sl1, sl_z = sl2, ls = '', rowlabels = ['T1 MR','OSEM','BOW','$BOW_{CNN}$'],
+                            imshow_kwargs = [mr_imshow_kwargs] + 3*[pet_imshow_kwargs])
+      vi.fig.savefig(os.path.splitext(prediction_file)[0] + '.png')
+      vi.fig_cb.savefig(os.path.splitext(prediction_file)[0] + '_cb.png')
+  
+      py.close(vi.fig)
+      py.close(vi.fig_cb)
+      py.close(vi.fig_sl)
+    else:
+      print('reading : ', df_file)
+      df = pd.read_csv(df_file)
+   
+    df['tracer'] = tracer 
+    reg_results = reg_results.append([df], ignore_index = True)
  
  
 #---------------------------------------------------------------------------------
 # filter background ROIs
 reg_results = reg_results.loc[(reg_results['region'] != 'other') & 
-                              (reg_results['region'] != 'ventricle') &
                               (reg_results['region'] != 'background')]
 
 #---------------------------------------------------------------------------------
@@ -209,22 +223,37 @@ reg_results = reg_results.loc[(reg_results['region'] != 'other') &
 
 fp = dict(marker = 'o', markerfacecolor = '0.3', markeredgewidth = 0, markersize = 2.5) 
 
-fig, ax = py.subplots(3,1, figsize = (12,8), sharex = True)
-sns.boxplot(x='region',  y ='rc_mean', data = reg_results, ax = ax[0], hue = 'dataset', flierprops = fp)
-sns.boxplot(x='region',  y ='ssim',    data = reg_results, ax = ax[1], hue = 'dataset', flierprops = fp)
-sns.boxplot(x='region',  y ='rmse',    data = reg_results, ax = ax[2], hue = 'dataset', flierprops = fp)
+fig, ax = py.subplots(2,1, figsize = (12,6), sharex = True)
+sns.boxplot(x='region',  y ='rc_mean', data = reg_results, ax = ax[0], hue = 'tracer', flierprops = fp)
+sns.boxplot(x='region',  y ='ssim',    data = reg_results, ax = ax[1], hue = 'tracer', flierprops = fp)
 for axx in ax: 
   axx.set_xticklabels(axx.get_xticklabels(),rotation=15)
   axx.grid(ls = ':')
 fig.tight_layout()
+fig.savefig(os.path.join('figs', 'regions_' + model_name.replace('.h5','.pdf')))
 fig.show()
 
-fig2, ax2 = py.subplots(3,1, figsize = (12,8), sharex = True)
-sns.boxplot(x='subject',  y ='rc_mean', data = reg_results, ax = ax2[0], hue = 'dataset', flierprops = fp)
-sns.boxplot(x='subject',  y ='ssim',    data = reg_results, ax = ax2[1], hue = 'dataset', flierprops = fp)
-sns.boxplot(x='subject',  y ='rmse',    data = reg_results, ax = ax2[2], hue = 'dataset', flierprops = fp)
+fig2, ax2 = py.subplots(2,1, figsize = (12,6), sharex = True)
+sns.boxplot(x='subject',  y ='rc_mean', data = reg_results, ax = ax2[0], hue = 'tracer', flierprops = fp)
+sns.boxplot(x='subject',  y ='ssim',    data = reg_results, ax = ax2[1], hue = 'tracer', flierprops = fp)
 for axx in ax2: 
   axx.set_xticklabels(axx.get_xticklabels(),rotation=90)
   axx.grid(ls = ':')
 fig2.tight_layout()
+fig2.savefig(os.path.join('figs', 'subjects_' + model_name.replace('.h5','.pdf')))
 fig2.show()
+
+# make the data tables
+sum_data = pd.DataFrame()
+for metric in ['rc_mean','ssim']:
+  for stat in [pd.DataFrame.median, pd.DataFrame.std, pd.DataFrame.min, pd.DataFrame.max]:
+    tmp = pd.DataFrame({(metric + ' ' + stat.__name__): reg_results.groupby(['tracer','region']).apply(stat)[metric]})
+    sum_data = pd.concat([sum_data, tmp], axis = 1)
+
+sum_data.to_latex(os.path.join('figs',model_name.replace('.h5','.tex')), float_format = '{:,.2f}'.format) 
+
+
+
+
+
+
