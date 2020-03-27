69c5522d010c0132b0262a8a9960d71e6c169b4f
Georg Schramm (VSC) <georg.schramm@kuleuven.be>
2020-03-27 16:33:27 +0100

    use scm_git_archiv to get correct version even from archive

--- setup.py
+++ setup.py
@@ -1,31 +1,13 @@
 import setuptools
-import os
-import subprocess
-import warnings
-
-# this gives only the correct path when using pip install -e
-pkg_dir = os.path.abspath(os.path.dirname(__file__))
-
-# in case the package is not a git repo but rather a release
-# we try to get the fallback for package version from the dirname
-tmp_split = pkg_dir.split('pyapetnet-')
-if len(tmp_split) >= 2:
-  fall_back_version = pkg_dir.split('pyapetnet-')[-1]
-else:
-  fall_back_version = 'unkown'
-
-with open(os.path.join(pkg_dir,"README.md"), "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name="pyapetnet",
-    use_scm_version={'fallback_version':fall_back_version},
-    setup_requires=['setuptools_scm'],
+    use_scm_version={'fallback_version':'unkown'},
+    setup_requires=['setuptools_scm','setuptools_scm_git_archive'],
     author="Georg Schramm",
     author_email="georg.schramm@kuleuven.be",
     description="A CNN to mimick anatomy guided PET reconstruction in image space",
     license='MIT',
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gschramm/pyapetnet",
     packages=setuptools.find_packages(),
