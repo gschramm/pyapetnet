d49da7ff72200173845fc5312e160580bd49486e
Georg Schramm <gschra2@stanford.edu>
2022-09-13 12:15:25 -0700

    add simple itk based registration module

--- pyapetnet/registration.py
+++ pyapetnet/registration.py
@@ -1,19 +1,75 @@
-# TODO return / save final transformation, init reg by translation
+import warnings
 import numpy as np
 import SimpleITK as sitk
 
-def affine_to_direction(aff: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    origin = aff[:-1,-1]
-    direction = aff[:-1,:-1]
-    spacing = np.linalg.norm(direction, axis = 0)
+
+def affine_to_direction(
+        aff: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """convert affine matrix to origin, spacing, direction
+
+    Parameters
+    ----------
+    aff : np.ndarray
+        affine matrix
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray, np.ndarray]
+        origin, spacing, direction
+    """
+    origin = aff[:-1, -1].copy()
+    direction = aff[:-1, :-1].copy()
+    spacing = np.linalg.norm(direction, axis=0)
     direction /= spacing
 
-    return origin, spacing, direction
+    return origin, spacing, direction.ravel()
+
+
+def affine_from_sitk_image(image: sitk.Image) -> np.ndarray:
+    """get the affine matrix from an sitk Image (header)
+
+    Parameters
+    ----------
+    image : sitk.Image
+
+    Returns
+    -------
+    np.ndarray
+        affine matrix
+    """
+    direction = np.array(image.GetDirection()).reshape(3, 3)
+    spacing = np.array(image.GetSpacing())
+    origin = np.array(image.GetOrigin())
+
+    affine = np.eye(4)
+    affine[:-1, :-1] = (direction * spacing)
+    affine[:-1, -1] = origin
+
+    return affine
+
 
 def array_to_sitk_image(arr: np.ndarray, aff: np.ndarray) -> sitk.Image:
+    """convert a 3D numpy array to an sitk Image
+
+    Parameters
+    ----------
+    arr : np.ndarray
+        3D array with axis order [x,y,z] or [LR,AP,FH]
+    aff : np.ndarray
+        affine matrix
+
+    Returns
+    -------
+    sitk.Image
+
+    Note
+    ----
+    By default sitk's GetImageFromArray assumes that the axis oder
+    is [z,y,x] which is why we swap axes before calling it.
+    """
     # sitk assumes that in numpy we have stores [z,y,x]
     # so we have to swap axes
-    img = sitk.GetImageFromArray(np.swapaxes(arr,0,2))
+    img = sitk.GetImageFromArray(np.swapaxes(arr, 0, 2))
     # remember to make a copy of affine, otherwise sITK operations will overwrite arrays
     origin, spacing, direction = affine_to_direction(aff.copy())
     img.SetOrigin(origin)
@@ -22,16 +78,46 @@ def array_to_sitk_image(arr: np.ndarray, aff: np.ndarray) -> sitk.Image:
 
     return img
 
+
 def sitk_image_to_array(img: sitk.Image) -> np.ndarray:
+    """get the array from an sitk Image
+
+    Parameters
+    ----------
+    img : sitk.Image
+
+    Returns
+    -------
+    np.ndarray
+
+    Note
+    ----
+    By default sitk's GetImageFromArray assumes that the axis oder
+    is [z,y,x] which is why we swap axes before calling it.
+    """
     # sitk assumes that in numpy we have stores [z,y,x]
     # so we have to swap axes
-    return np.swapaxes(sitk.GetArrayFromImage(img),0,2)
+    return np.swapaxes(sitk.GetArrayFromImage(img), 0, 2)
 
 
 def resample_sitk_image(volume: sitk.Image,
-                    new_spacing: tuple[float, float, float],
-                    interpolator: int = sitk.sitkLinear) -> sitk.Image:
-    """ resample volume to differnt voxel size"""
+                        new_spacing: tuple[float, float, float],
+                        interpolator: int = sitk.sitkLinear) -> sitk.Image:
+    """Resample an sitk image to a new spacing (voxel size)
+
+    Parameters
+    ----------
+    volume : sitk.Image
+    new_spacing : tuple[float, float, float]
+        the new spacing (voxel size)    
+    interpolator : int, optional
+        interpolator to use, by default sitk.sitkLinear
+
+    Returns
+    -------
+    sitk.Image
+        the resampled image
+    """
     original_spacing = volume.GetSpacing()
     original_size = volume.GetSize()
     new_size = [
@@ -42,27 +128,62 @@ def resample_sitk_image(volume: sitk.Image,
                          volume.GetOrigin(), new_spacing,
                          volume.GetDirection(), 0, volume.GetPixelID())
 
-def align_and_resample(fixed_image: sitk.Image | np.ndarray,
-                       moving_image: sitk.Image | np.ndarray,
-                       fixed_affine: np.ndarray | None = None,
-                       moving_affine: np.ndarray | None = None,
-                       new_spacing: tuple[float, float, float] = (1., 1., 1.),
-                       sampling_rate: float = 0.01,
-                       init_mode: sitk.Transform | str | None = 'MOMENTS',
-                       registration_method: sitk.ImageRegistrationMethod | None = None,
-                       seed: int = 0,
-                       verbose: bool = False) -> tuple[sitk.Image, sitk.Image]:
-    """ align two SITK images and interpolate to a nomimal voxel spacing if needed """
 
-    if isinstance(fixed_image, np.ndarray):
-        if fixed_affine is None:
-            raise TypeError('affine must be given if input is np.ndarray')
-        fixed_image = array_to_sitk_image(fixed_image, fixed_affine)
+def align_and_resample(
+    fixed_image: np.ndarray,
+    moving_image: np.ndarray,
+    fixed_affine: np.ndarray,
+    moving_affine: np.ndarray,
+    new_spacing: tuple[float, float, float] = (1., 1., 1.),
+    sampling_rate: float = 0.01,
+    init_mode: sitk.Transform | str = 'MOMENTS',
+    registration_method: sitk.ImageRegistrationMethod | None = None,
+    final_transform: sitk.Transform | None = None,
+    verbose: bool = False
+) -> tuple[np.ndarray, np.ndarray, sitk.Transform, np.ndarray]:
+    """wrapper to align and resample two 3D images using simple ITK
 
-    if isinstance(moving_image, np.ndarray):
-        if moving_affine is None:
-            raise TypeError('affine must be given if input is np.ndarray')
-        moving_image = array_to_sitk_image(moving_image, moving_affine)
+    Parameters
+    ----------
+    fixed_image : np.ndarray
+        the fixed 3D image
+    moving_image : np.ndarray
+        the moving 3D image
+    fixed_affine : np.ndarray
+        affine maxtrix for fixed image
+    moving_affine : np.ndarray
+        affine maxtrix for moving image
+    new_spacing : tuple[float, float, float], optional
+        the new voxel size after resampling, by default (1., 1., 1.)
+    sampling_rate : float, optional
+        random sampling rate used in registration, by default 0.01
+    init_mode : sitk.Transform | str, optional
+        initialization mode for transformation, by default 'MOMENTS'
+        'MOMENTS' -> align moments of images
+        'GEOMETRY' -> align FOV centers
+        sitk.Transform -> use this transform
+    registration_method : sitk.ImageRegistrationMethod | None, optional
+        sitk registration method to use, by default None
+        None -> use mattes mutual information and conjugate gradient optimizer
+    final_transform : sitk.Transform | None, optional
+        final transformation (e.g. loaded from file), by default None
+        None -> calculate final transformation that maps moving image to
+                resampled fixed image
+    verbose : bool, optional
+        print verbose output, by default False
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray, sitk.Transform, np.ndarray]
+        - the resampled fixed image 
+        - the aligned moving image, the final transform that maps the moving image to 
+        - the resampled fixed image 
+          the affine matrix of the
+        - resampled fixed image
+    """
+
+    fixed_image = array_to_sitk_image(fixed_image, fixed_affine)
+    moving_image = array_to_sitk_image(moving_image, moving_affine)
 
     if not tuple(new_spacing) == fixed_image.GetSpacing():
         fixed_image = resample_sitk_image(fixed_image, new_spacing)
@@ -80,16 +201,17 @@ def align_and_resample(fixed_image: sitk.Image | np.ndarray,
             sitk.CenteredTransformInitializerFilter.GEOMETRY)
     elif isinstance(init_mode, sitk.Transform):
         initial_transform = init_mode
-    elif init_mode is None:
-        # get the initial transform based on the affine information from the header 
-        # not working yet
-        resampler = sitk.ResampleImageFilter()
-        resampler.SetReferenceImage(fixed_image)
-        _ = resampler.Execute(moving_image)
-        initial_transform = resampler.GetTransform()
     else:
-        raise ValueError('initial tranform must be either None, GEOMETRY, MOMENTS or sitk.transform')
-
+        raise ValueError(
+            'initial tranform must be either None, GEOMETRY, MOMENTS or sitk.transform'
+        )
+    #elif init_mode is None:
+    #    # get the initial transform based on the affine information from the header
+    #    # not working yet
+    #    resampler = sitk.ResampleImageFilter()
+    #    resampler.SetReferenceImage(fixed_image)
+    #    _ = resampler.Execute(moving_image)
+    #    initial_transform = resampler.GetTransform()
 
     # Registration
     if registration_method is None:
@@ -105,11 +227,12 @@ def align_and_resample(fixed_image: sitk.Image | np.ndarray,
         registration_method.SetInterpolator(sitk.sitkLinear)
 
         # Optimizer settings.
-        registration_method.SetOptimizerAsGradientDescentLineSearch(
+        registration_method.SetOptimizerAsConjugateGradientLineSearch(
             learningRate=1.,
             numberOfIterations=100,
             convergenceMinimumValue=1e-6,
             convergenceWindowSize=10)
+        #registration_method.SetOptimizerAsLBFGSB()
 
         if (init_mode == 'GEOMETRY') or (init_mode == 'MOMENTS'):
             registration_method.SetOptimizerScalesFromPhysicalShift()
@@ -120,9 +243,16 @@ def align_and_resample(fixed_image: sitk.Image | np.ndarray,
             smoothingSigmas=[2, 1, 0])
         registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 
-        registration_method.SetInitialTransform(initial_transform, inPlace=False)
+        registration_method.SetInitialTransform(initial_transform,
+                                                inPlace=False)
 
-    final_transform = registration_method.Execute(fixed_image, moving_image)
+    if final_transform is None:
+        try:
+            final_transform = registration_method.Execute(
+                fixed_image, moving_image)
+        except:
+            warnings.warn('SITK registation failed. Using initial transform')
+            final_transform = initial_transform
 
     # Post registration analysis
     if verbose:
@@ -136,25 +266,6 @@ def align_and_resample(fixed_image: sitk.Image | np.ndarray,
                                          final_transform, sitk.sitkLinear, 0.0,
                                          moving_image.GetPixelID())
 
-    return fixed_image, moving_image_aligned
-
-if __name__ == '__main__':
-    reader = sitk.ImageSeriesReader()
-    
-    pet_image = sitk.ReadImage('pet.nii', outputPixelType=sitk.sitkFloat32)
-    mr_image = sitk.ReadImage('flair.nii', outputPixelType=sitk.sitkFloat32)
-
-    from utils import load_nii_in_ras
-    pet_vol, pet_aff = load_nii_in_ras('pet.nii')
-    mr_vol, mr_aff = load_nii_in_ras('flair.nii')
-
-    pet_vol = np.roll(pet_vol, 25, 0)
-
-    a, b = align_and_resample(mr_vol, pet_vol, fixed_affine=mr_aff, moving_affine=pet_aff, 
-                             new_spacing=(0.9,0.9,0.9), verbose = True)
-
-    c, d = align_and_resample(mr_vol, pet_vol, fixed_affine=mr_aff, moving_affine=pet_aff, 
-                             new_spacing=(0.9,0.9,0.9), verbose = True)
-
-    import pymirc.viewer as pv
-    vi = pv.ThreeAxisViewer([sitk.GetArrayFromImage(c), sitk.GetArrayFromImage(d)])
+    return sitk_image_to_array(fixed_image), sitk_image_to_array(
+        moving_image_aligned), final_transform, affine_from_sitk_image(
+            fixed_image)
